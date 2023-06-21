eec9d700684be46e2ab12f85a04fb601fa204f03
Hieulunm61 <106015669+Hieulunm61@users.noreply.github.com>
2023-06-21 08:28:00 +0700

    Update database.js

--- database.js
+++ database.js
@@ -2,4 +2,3 @@ import * as database from "./datastructure.js";
 console.log(database.sort([10, 9, 8]));
 console.log(database.sort([10, 9, 8]));
 console.log("Tao là goodog mới edit this comment");
-console.log("Hôm nay tôi sẽ học datastructure and algorithm");
