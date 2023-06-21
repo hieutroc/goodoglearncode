ffa0354adaae58333acded29f76658320098cc85
hieutroc <103368155+hieutroc@users.noreply.github.com>
2023-06-21 08:32:53 +0700

    Update database.js

--- database.js
+++ database.js
@@ -1,4 +1,3 @@
 import * as database from "./datastructure.js";
 console.log(database.sort([10, 9, 8]));
-console.log(database.sort([10, 9, 8]));
-console.log("Tao là goodog mới edit this comment");
+
