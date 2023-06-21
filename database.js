08db8b63e12541e07313d62f64415b67da15bd40
Hieulunm61 <106015669+Hieulunm61@users.noreply.github.com>
2023-06-21 08:16:23 +0700

    database

--- database.js
+++ database.js
@@ -1,4 +1,5 @@
-import * as database from './datastructure.js';
-console.log(database.sort([10,9,8])); 
-console.log(database.sort([10,9,8])); 
-console.log('Tao là goodog mới edit this comment')
+import * as database from "./datastructure.js";
+console.log(database.sort([10, 9, 8]));
+console.log(database.sort([10, 9, 8]));
+console.log("Tao là goodog mới edit this comment");
+console.log("Hôm nay tôi sẽ học datastructure and algorithm");
